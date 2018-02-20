42a3d6068f9bbc2c091e7ce1b016421cf6b3b1cc
Ryan Ferris and Zach Evans <zach@>
2018-02-20 09:38:20 -0800

    Finish Wednesday material.

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -3,10 +3,19 @@ import { Task } from './task.model';
 
 @Component({
 	selector: 'task-list',
-	template: `
-  <ul>
-     <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
-  </ul>
+  template: `
+    <select (change)="onChange($event.target.value)">
+      <option value="allTasks">All Tasks</option>
+      <option value="completedTasks">Completed Tasks</option>
+      <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+    </select>
+    <ul>
+      <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
+        <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+        <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+        <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button>
+      </li>
+    </ul>
   `
 })
 
@@ -14,15 +23,25 @@ export class TaskListComponent {
 	@Input() childTaskList: Task[];
 	@Output() clickSender = new EventEmitter();
 
+	onChange(optionFromMenu) {
+  this.filterByCompleteness = optionFromMenu;
+}
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+     clickedTask.done = setCompleteness;
+   }
+	 
 	editButtonHasBeenClicked(taskToEdit: Task) {
 		this.clickSender.emit(taskToEdit);
 	}
 
+	filterByCompleteness: string = "incompleteTasks";
+
 	isDone(clickedTask: Task) {
 		if (clickedTask.done === true) {
-			alert("This task is done!");
+			console.log("This task is done!");
 		} else {
-			alert("This task is not done. Better get to work!");
+			console.log("This task is not done. Better get to work!");
 		}
 	};
 
